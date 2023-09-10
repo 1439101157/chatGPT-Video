81d5a50c71d9d8fd9c03b66ef86e3512a07cfb64
1439101157 <149101157@qq.com>
2023-09-10 11:41:46 +0800

    现在可以读取代码文件了

---
+++ src/main/java/com/example/gptvideohelper/api/GetFileContent.java
@@ -0,0 +1,29 @@
+package com.example.gptvideohelper.api;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import cn.hutool.core.io.file.FileReader;
+public class GetFileContent {
+
+    public static void main(String[] args) throws IOException {
+        // 创建一个BufferedReader对象，用来读取txt文件中的每一行路径
+        FileReader reader =new FileReader("D:\\project\\chat-gpt-video\\filetree.txt");
+        // 创建一个BufferedWriter对象，用来写入另一个txt文本中
+        BufferedWriter writer = new BufferedWriter(new FileWriter("D:\\project\\chat-gpt-video\\filecontent.txt"));
+        // 定义一个字符串变量，用来存储每一行路径
+        List<String> lineList = reader.readLines();
+        // 使用循环，逐行读取路径，并判断是否为空
+        for(String line : lineList) {
+            // 如果路径不为空，就创建一个FileInputStream对象，用来读取该路径对应的文件内容
+            FileReader fis = new FileReader(line);
+            // 使用BufferedWriter.write()方法，将字节数组转换成字符串，并写入到另一个txt文本中
+            writer.write(fis.readString());
+            // 在每个文件内容之后，添加一个换行符，以便区分不同的文件
+            writer.newLine();
+        }
+        writer.close();
+    }
+}
+
---
+++ src/main/java/com/example/gptvideohelper/api/GetFileTree.java
@@ -0,0 +1,50 @@
+package com.example.gptvideohelper.api;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class GetFileTree {
+    public static void crawlFiles(File file, BufferedWriter writer, int level) throws IOException {
+
+        // 获取文件夹下的所有文件或子文件夹
+        File[] files = file.listFiles();
+        // 遍历数组
+        for (File f : files) {
+            // 如果是文件，就判断是否是Java代码文件
+            if (f.isFile()) {
+                // 获取文件名
+                String fileName = f.getName();
+                // 获取文件后缀名
+                String suffix = fileName.substring(fileName.lastIndexOf(".") + 1);
+                // 定义一个数组，存储Java开发中会用到的各种文件后缀
+                String[] suffixes = {"java", "xml", "properties", "sql", "jsp", "html", "css", "js"};
+                // 判断文件后缀名是否在数组中
+                boolean isJavaFile = Arrays.asList(suffixes).contains(suffix);
+                // 如果是Java代码文件，就将其绝对路径写入到txt文件中，并根据层级深度添加缩进
+                if (isJavaFile) {
+                    for (int i = 0; i < level; i++) {
+                        writer.write("\t");
+                    }
+                    // 替换\为\\
+                    String path = f.getAbsolutePath().replace("\\", "\\\\");
+                    writer.write(path);
+                    writer.newLine();
+                }
+            } else if (f.isDirectory()) {
+                // 如果是子文件夹，就判断是否是.git或者.idea文件夹
+                if (!f.getName().equals(".git") && !f.getName().equalsIgnoreCase(".idea")&&!f.getName().equals(".mvn")) {
+                    // 如果不是.git或者.idea文件夹，就继续递归遍历，并将层级深度加一
+                    crawlFiles(f, writer, level + 1);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        File folder = new File("D:\\project\\chat-gpt-video");
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("D:\\project\\chat-gpt-video\\filetree.txt"), "UTF-8"));
+        crawlFiles(folder, writer, 0);
+        writer.close();
+
+    }
+}
